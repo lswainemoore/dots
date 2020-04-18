de7bc1d3954ffd1b9df877441319075bd8c6dd40
lsm <lswainemoore@gmail.com>
2020-04-18 17:57:31 -0400

    bc i already forgot this

--- linkify.sh
+++ linkify.sh
@@ -1,5 +1,8 @@
 #/bin/bash
 
+# this should be run from its local directory
+# so it links the files to the appropriate places
+
 pwd=$(pwd)
 check_and_link () {
   # this is a lie. it doesn't actually check rn
