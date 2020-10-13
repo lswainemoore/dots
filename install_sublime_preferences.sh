450beb295181fa79236ea63941a76fef4209e90f
lincoln <lswainemoore@gmail.com>
2020-10-13 02:59:41 -0400

    command for linking sublime

---
+++ install_sublime_preferences.sh
@@ -0,0 +1,13 @@
+HOME_DIR=~/
+SUBLIME_DIR="/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/"
+DOTS_DIR=/dots/
+PREF_FILE=Preferences.sublime-settings
+KEYMAP_FILE="Default\ \(OSX\).sublime-keymap"
+echo "remove main preferences"
+echo "rm $HOME_DIR$SUBLIME_DIR$PREF_FILE"
+echo "linking main preferences"
+echo "ln -s $HOME_DIR$DOTS_DIR$PREF_FILE $HOME_DIR$SUBLIME_DIR$PREF_FILE"
+echo "removing keymap"
+echo "rm $HOME_DIR$SUBLIME_DIR$KEYMAP_FILE"
+echo "linking keymap"
+echo "ln -s $HOME_DIR$DOTS_DIR$KEYMAP_FILE $HOME_DIR$SUBLIME_DIR$KEYMAP_FILE"
